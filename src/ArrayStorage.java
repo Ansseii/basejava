4d8cf1153edc545c2e1e967a4c060e8953019ad5
Ansseii <a.lukash.92@gmail.com>
2018-06-25 22:22:24 +0300

    ArrayStorage has been updated

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    private Resume[] storage = new Resume[10000];
+    private int count = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        count = 0;
     }
 
-    void save(Resume r) {
+    void save(final Resume r) {
+        storage[count++] = r;
     }
 
-    Resume get(String uuid) {
+    Resume get(final String uuid) {
+
+        for (Resume i : storage) {
+            if (uuid.equals(i.uuid)) return i;
+            else break;
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    void delete(final String uuid) {
+
+        int i;
+        for (i = 0; i < count; i++) {
+            if (uuid.equals(storage[i].uuid)) break;
+        }
+        for (int j = i; j < count - 1; j++) {
+            storage[j] = storage[j + 1];
+        }
+        count--;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
+
