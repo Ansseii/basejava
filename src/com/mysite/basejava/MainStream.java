f7355902744e66f41694f228fa7d4a6fa49ea917
Ansseii <a.lukash.92@gmail.com>
2018-08-24 13:59:35 +0300

    Optional HW Stream minValue

---
+++ src/com/mysite/basejava/MainStream.java
@@ -0,0 +1,24 @@
+package com.mysite.basejava;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MainStream {
+
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{1, 2, 3, 3, 2, 3};
+        System.out.println(minValue(array));
+
+    }
+
+    private static int minValue(final int[] values) {
+
+    return Integer.parseInt(Arrays.stream(values)
+            .distinct()
+            .sorted()
+            .mapToObj(Integer::toString)
+            .collect(Collectors.joining()));
+    }
+}
