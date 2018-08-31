c831dd7838b393be8143badb7af7c614a0e7d31c
Ansseii <a.lukash.92@gmail.com>
2018-08-31 14:04:15 +0300

    HW 13 update

--- src/com/mysite/basejava/storage/SqlStorage.java
+++ src/com/mysite/basejava/storage/SqlStorage.java
@@ -32,7 +32,6 @@ public class SqlStorage implements Storage {
             if (preparedStatement.executeUpdate() == 0) {
                 throw new NotExistStorageException(resume.getUuid());
             }
-            preparedStatement.execute();
             return null;
         });
     }
@@ -66,7 +65,6 @@ public class SqlStorage implements Storage {
             if (preparedStatement.executeUpdate() == 0) {
                 throw new NotExistStorageException(uuid);
             }
-            preparedStatement.execute();
             return null;
         });
     }
